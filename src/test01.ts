daefd171569f4c9eab8d1d7d5dca7f879aac0611
Roxanne Viette <74608393+Roxanne2904@users.noreply.github.com>
2024-01-17 16:43:19 +0100

    test01 (#1)

---
+++ src/test01.ts
@@ -0,0 +1,75 @@
+// Quel est le type de données de la variable "age" ?
+const age = 25; // number to be more specific is 25 type.
+console.log("age:", `${age}: number`);
+
+// Créez une interface "Person" avec les propriétés "name" de type chaîne de caractères et "age" de type nombre.
+interface Person {
+	name: string;
+	age: number;
+}
+const person: Person = {
+	name: "John",
+	age: 23,
+};
+console.log(
+	"person:",
+	`${person}: {
+	name: string,
+	age: number,
+}`
+);
+// Écrivez une fonction appelée "add" qui prend deux paramètres de type nombre et renvoie leur somme.
+
+const add = (arg1: number, arg2: number): number => {
+	return arg1 + arg2;
+};
+
+function add01<T extends number, U extends number>(arg1: T, arg2: U): number {
+	return arg1 + arg2;
+}
+
+console.log("add function", add(23, 45));
+console.log("add01 function", add01<67, 45>(67, 45));
+
+// Créez une classe "Animal" avec une propriété "name" de type chaîne de caractères et une méthode "makeSound" qui renvoie une chaîne de caractères.
+
+type Animals = "horse" | "pig" | "firefly" | "cat" | "dog";
+
+class Animal {
+	public name: Animals;
+
+	constructor(name: Animals) {
+		this.name = name;
+	}
+
+	makeSound(): string {
+		return `you've chosen a ${this.name}`;
+	}
+}
+
+console.log("class Animal", new Animal("horse").name);
+console.log("class Animal", new Animal("pig").makeSound);
+
+// Écrivez une fonction générique appelée "reverseArray" qui prend un tableau de n'importe quel type et renvoie le tableau inversé.
+
+function reverseArray<T>(array: T[]): T[] {
+	return array.reverse();
+}
+
+const originalArray = [1, 2, 3, 4, 5, 6];
+console.log("reverseArray", reverseArray([originalArray]));
+
+// Utilisez les promesses pour simuler une opération asynchrone qui résout une chaîne de caractères après 2 secondes.
+
+const simulatedAsyncOperation = (): Promise<string> =>
+	new Promise((resolve: (arg: string) => void) => {
+		setTimeout(
+			() => resolve("Opération asynchrone réussie après 2 secondes"),
+			2000
+		);
+	});
+
+console.log(
+	"simulatedAsyncOperation",
+	simulatedAsyncOperation().then((result) => console.log(result))
+);
